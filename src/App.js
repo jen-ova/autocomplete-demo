0db9cb7598e95cfd995302ffe82f224f635248a0
Jen Stewart <jenstewart2404@gmail.com>
2022-02-15 17:14:47 +0000

    fixed css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Autocomplete from "./components/Autocomplete";
 import AutocompleteProvider from "./components/store/AutocompleteProvider";
-import "./app.css";
+import "./App.css";
 
 const App = () => {
   return (
